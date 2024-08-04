88a1d849a69aa3bb78f8958fdcdff3a2412cf593
Vishnu200399 <hackingup3@gmail.com>
2024-08-04 19:32:55 +0530

    second commit

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,40 +7,20 @@ import { Router, NavigationEnd } from '@angular/router';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  sidebarActive: boolean = false;
+
+  toggleSidebar() {
+    this.sidebarActive = !this.sidebarActive;
+  }
+
+
+  set() {
+    // Additional logic for setting state if needed
+  }
+
 
   constructor(private router: Router) { }
-  set(){
-    // let name = String(this.name);
-    let data ={ id:1,name:'reddy'};
-          localStorage.setItem("name", JSON.stringify(data));
-          console.log("true");
-  }
   
   ngOnInit(): void {
-    this.router.events.subscribe((event) => {
-      if (event instanceof NavigationEnd) {
-        const headerElement = document.getElementById('header');
-        if (headerElement) {
-          headerElement.classList.remove('login');
-          headerElement.classList.remove('password');
-          headerElement.classList.remove('signup');
-          headerElement.classList.remove('admin');
-          headerElement.classList.remove('user');
-
-          if (event.url === '/login') {
-            headerElement.classList.add('login');
-          } else if (event.url === '/password') {
-            headerElement.classList.add('password');
-          } else if (event.url === '/signup') {
-            headerElement.classList.add('signup');
-          }else if (event.url === '/admin') {
-            headerElement.classList.add('admin');
-          }else if (event.url === '/user') {
-            headerElement.classList.add('user');
-          }
-
-        }
-      }
-    });
   }
 }
